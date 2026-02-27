119291a4226d8343dec0ecefbb91d9c217c5d0cc
Dion Leung <dioncleung@gmail.com>
2026-02-26 17:32:51 -0800

    moving post routes for years

--- src/api/controllers/YearsController/index.js
+++ src/api/controllers/YearsController/index.js
@@ -1,61 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
 const db = require("../../models/index.js").default;
 
-const { buildYearData } = require("../../util/utilities");
+// endpoint is POST(/years/one)
+router.post("/one", async (req, res) => {
+  console.log("Creating a new season...");
 
-// endpoint is GET(/years)
-// GET all Seasons in database
-router.get("/", async (req, res) => {
-  console.log("Retrieving all seasons from the database...");
+  console.log(req.body);
 
   try {
-    db.sequelize.models.Season.findAll().then(seasons => {
-      console.log("Found all seasons");
-      res.status(200).send(seasons);
+    db.sequelize.models.Season.create(req.body).then(seasons => {
+      console.log(
+        "New seasons created successfully - query the database to see them",
+      );
+      console.log(seasons);
+      res.status(200).send({
+        message: "Successfully created new seasons.",
+        newSeasons: seasons,
+      });
     });
   } catch (err) {
-    console.error("Error retrieving seasons from the database:", err);
+    console.error("Error creating new seasons:", err);
     res
       .status(500)
-      .send({ error: "An error occurred while retrieving seasons." });
+      .send({ error: "An error occurred while creating the seasons." });
+    return;
   }
 });
 
-// endpoint is GET(/years/:year)
-router.get("/:year", async (req, res) => {
-  const { year } = req.params;
+// endpoint is POST(/years/many)
+router.post("/many", async (req, res) => {
+  console.log("Creating a new set of new seasons...");
 
-  let yearData;
+  console.log(req.body);
 
   try {
-    yearData = fs.readFileSync(
-      `${process.cwd()}/src/resources/sample_payloads/years/mrhs-${year}.json`,
-      (err, jsonData) => {
-        if (err) {
-          console.log("oh nooooooooo");
-          console.error(err);
-          return null;
-        }
-        console.log(`reading ${year} data successfull`);
-        let year = JSON.parse(jsonData);
-        return year;
-      },
-    );
+    db.sequelize.models.Season.bulkCreate(req.body).then(seasons => {
+      console.log(
+        "New seasons created successfully - query the database to see them",
+      );
+      res.status(200).send({
+        message: "Successfully created new seasons.",
+        newSeasons: seasons,
+      });
+    });
   } catch (err) {
-    console.log("Error with readFileSync");
-    if (err.code === "ENOENT") {
-      console.log("Year data file not found, proceeding without it.");
-    }
-    console.error(err);
-    yearData = null;
-  }
-
-  if (yearData !== null) {
-    res.status(200).send(buildYearData(yearData));
-  } else {
-    res.status(500).send({ message: "Year data not found" });
+    console.error("Error creating new seasons:", err);
+    res
+      .status(500)
+      .send({ error: "An error occurred while creating the seasons." });
+    return;
   }
 });
 
