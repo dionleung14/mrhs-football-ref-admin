b9155fe49b260eb995ffdcea94cfa581840599d3
Dion Leung <dioncleung@gmail.com>
2026-02-27 22:49:31 -0800

    added bulk create for games

--- src/api/controllers/GamesController/index.js
+++ src/api/controllers/GamesController/index.js
@@ -4,8 +4,8 @@ const db = require("../../models/index.js").default;
 
 // endpoint is POST(/games/one)
 router.post("/one", async (req, res) => {
-  console.log("Trying to hit the test slash route with a POST");
-  // console.log(req.body.points_allowed_overtime);
+  console.log(`${req.method} request received to ${req.originalUrl}`);
+
   const {
     season,
     date,
@@ -47,7 +47,7 @@ router.post("/one", async (req, res) => {
       pointsAgainstOT: points_allowed_overtime,
     }).then(game => {
       console.log("New game created successfully:");
-      console.log(game.toJSON());
+      // console.log(game.toJSON());
       res.status(200).send({
         test: "Successfully created a new game.",
         newGame: game.toJSON(),
@@ -58,6 +58,52 @@ router.post("/one", async (req, res) => {
     res
       .status(500)
       .send({ error: "An error occurred while creating the game." });
+    return;
+  }
+});
+
+// endpoint is POST(/games/many)
+router.post("/many", async (req, res) => {
+  console.log(`${req.method} request received to ${req.originalUrl}`);
+
+  // const {
+  //   season,
+  //   date,
+  //   opponent,
+  //   home,
+  //   win_loss,
+  //   points_scored_quarter_1,
+  //   points_scored_quarter_2,
+  //   points_scored_quarter_3,
+  //   points_scored_quarter_4,
+  //   total_points_scored,
+  //   points_allowed_quarter_1,
+  //   points_allowed_quarter_2,
+  //   points_allowed_quarter_3,
+  //   points_allowed_quarter_4,
+  //   total_points_allowed,
+  //   points_scored_overtime,
+  //   points_allowed_overtime,
+  // } = req.body;
+
+  try {
+    await db.sequelize.models.Game.bulkCreate(req.body, {
+      validate: true,
+    }).then(games => {
+      console.log(
+        "New games created successfully, query the database to see them",
+      );
+      // console.log(game.toJSON());
+      res.status(200).send({
+        test: "Successfully created new games.",
+        newGames: games.map(game => game.toJSON()),
+      });
+    });
+  } catch (err) {
+    console.error("Error creating new games:", err);
+    res
+      .status(500)
+      .send({ error: "An error occurred while creating the games." });
     return;
   }
 });
